39e34d5425981478760f3c5248dbe454fdf5a7b1
ChenHuang <alan.tolearn@gmail.com>
2020-08-15 17:42:45 -0400

    Add auth token to collect function

--- lib/collect.py
+++ lib/collect.py
@@ -2,6 +2,15 @@ import os
 import json
 
 def collect(event, context):
+    if not verify_token(event):
+        return {
+            'statusCode': 401,
+            'body': 'Invalid Token',
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
+
     print(event['body'])
     return {
         'statusCode': 200,
@@ -10,3 +19,7 @@ def collect(event, context):
             'Content-Type': 'application/json'
         }
     }
+
+def verify_token(event):
+    authorization_token = event.get('headers', {}).get('Authorization')
+    return authorization_token == 'Basic ' + os.environ.get("AUTH_TOKEN")
